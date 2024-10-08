2604933cec87462786bbc670718c9593dd766f91
G0rocks <huldarh@gmail.com>
2024-10-08 15:15:14 +0000

    main.py init

---
+++ main.py
@@ -0,0 +1,39 @@
+'''
+Authors:    Ragnar, Danielle and Hulda
+Date:       2024-10-08
+Project:
+Neural network that solves the kaggle used car prices problem:
+https://www.kaggle.com/competitions/playground-series-s4e9/data
+
+Solves using a single layer neural network, with regression and a basis function as per chapter 4 in Bishop 
+A special interest on missing data
+'''
+
+# Load data
+
+
+# Transform data into manageable form
+
+
+
+# Split data into training_data, training_targets, test_data & test_targets
+
+
+# Initialize neural network
+
+
+# Train neural network on training set
+
+
+# Test neural network on test set, log errors
+
+
+# Plot results
+
+
+# For when the above code is already working, implement the following:
+#-----------------------------------------
+# Train with complete data, test with complete data
+# Train with complete data, test with incomplete data
+# Train with incomplete data, test with complete data
+# Train with incomplete data, test with incomplete data
