2f5c578744a4a4e86c5932fb959892878318a787
G0rocks <huldarh@gmail.com>
2024-10-14 12:18:35 +0000

    activation function implemented

--- main.py
+++ main.py
@@ -8,30 +8,53 @@ https://www.kaggle.com/competitions/playground-series-s4e9/data
 Solves using a single layer neural network, with regression and a basis function as per chapter 4 in Bishop 
 A special interest on missing data
 '''
+print("\nRunning main.py")
 # Imports
 import torch
 import tools # Group tools
 
 # Load data
+#print("Loading data")
 
+
+
+#print("Complete\n")
 
 # Transform data into manageable form
 
 
 
 # Split data into training_data, training_targets, test_data & test_targets
+#print("Splitting data")
 
+
+
+#print("Complete\n")
 
 # Initialize neural network
 
 
 # Train neural network on training set
+#print("Training neural network")
+
+
+
+#print("Complete\n")
 
 
 # Test neural network on test set, log errors
+#print("Testing neural network")
 
+
+
+#print("Complete\n")
 
 # Plot results
+#print("Plotting results")
+
+
+
+#print("Complete\n")
 
 
 # For when the above code is already working, implement the following:
@@ -40,3 +63,16 @@ import tools # Group tools
 # Train with complete data, test with incomplete data
 # Train with incomplete data, test with complete data
 # Train with incomplete data, test with incomplete data
+
+# Testing area
+#-----------------------------------------------
+x = torch.ones(3)
+w = torch.rand(3)
+w = w-0.5
+print("activation function test")
+print("w: " + str(w))
+print(tools.h(x,w))
+
+# Run finished message
+print("\n")
+print("main.py ran succesfully")
--- tools.py
+++ tools.py
@@ -6,6 +6,7 @@ Background functions for helping solve the kaggle used car prices competition:
 https://www.kaggle.com/competitions/playground-series-s4e9/data
 '''
 # Imports
+import torch
 
 # Read data function
 
@@ -16,4 +17,24 @@ https://www.kaggle.com/competitions/playground-series-s4e9/data
 
 # Measure error function
 
+
+# Activation function, h
+def h(z1: torch.Tensor, w1: torch.Tensor) -> float:
+    '''
+    ReLU neuron activation function.
+    The same function is used for each neuron.
+    Does a linear combination of the previous layers outputs and the weights between those nodes and this one
+    
+    inputs:
+    z1  :   Size (1 x D), the output of the last layer.
+    w1  :   Size (D x 1), the weights for this neuron.
+    
+    outputs:
+    z2  :   the ReLU of the dot product of z1 and w1, the output of each node of this layer
+    '''
+    # Get weighed sum (dot product) of z1 and w1
+    w_sum = torch.matmul(z1,w1)
+    # ReLU of the weighed sum
+    ReLU = torch.nn.ReLU()
+    return ReLU(w_sum).item() # .item() pulls the 1x1 tensor value out
 
