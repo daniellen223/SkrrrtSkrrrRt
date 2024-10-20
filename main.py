36af6ca61f63dc036e62a2dd2fd08065ac156d56
G0rocks <huldarh@gmail.com>
2024-10-20 23:17:23 +0000

    main.py now has timer and cool progress messages

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ https://www.kaggle.com/competitions/playground-series-s4e9/data
 Solves using a single layer neural network, with regression and a basis function as per chapter 4 in Bishop 
 A special interest on missing data
 '''
-print("\nRunning main.py")
-# Imports
-import torch
-import tools # Group tools
-import time
-
+# Start timer import
+import time # For measuring runtime
 # Start timer
 start_time = time.time()
 
+# Start message
+print(" \r\nRunning main.py")
+
+# Imports
+import torch
+import tools # Group tools
+
 # Load data
-print("Loading data.....",end="")
+print("Loading data..............",end="")
 data, targets = tools.read_in_file()
 print("Complete")
 
@@ -27,32 +30,26 @@ print("Complete")
 
 
 # Split data into training_data, training_targets, test_data & test_targets
-print("Splitting data...",end="")
+print("Splitting data............",end="")
 (train_data, train_targets), (test_data, test_targets) = tools.split_data(data,targets, train_ratio=0.1)
 print("Complete")
 
 # Initialize neural network
-
+# torch.nn or similar might be useful for this - Huldar 2024-10-20
 
 # Train neural network on training set
 #print("Training neural network...",end="")
-
-
 
 #print("Complete")
 
 
 # Test neural network on test set, log errors
-#print("Testing neural network...",end="")
-
-
+#print("Testing neural network....",end="")
 
 #print("Complete")
 
 # Plot results
-#print("Plotting results...",end="")
-
-
+#print("Plotting results..........",end="")
 
 #print("Complete")
 
@@ -66,20 +63,7 @@ print("Complete")
 
 # Testing area
 #-----------------------------------------------
-'''
-x = torch.rand(10, 3)
-t = torch.rand(10)
-print("testing data split")
-print("data: \n" + str(x))
-print("Targets: \n" + str(t))
-(train_data, train_targets), (test_data, test_targets) = tools.split_data(x,t, train_ratio=0.1)
-print("Train_data: \n" + str(train_data))
-print("Train_Targets: \n" + str(train_targets))
-print("Test_data: \n" + str(test_data))
-print("Test_Targets: \n" + str(test_targets))
-'''
 
-# print(tools.init_weights(3, 5))
 
 # Calculate runtime
 runtime = time.time() - start_time
