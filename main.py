Change error_to_csv() function to tensor_to_csv() function to make it more universal and fix data normalization

main.py
@@ -22,11 +22,12 @@ print(" \r\nRunning main.py")
 
 # Settings
 #--------------------------------------------------------
-data_file_name = "train.csv"    # Which file to get the data from
+data_file_name = "Data_Normalized.csv"    # Which file to get the data from
 save_mapping = True            # Saves the data mapping for the read data to a csv file
 mapping_folder = "Data_mapping" # Name of mapping folder
+normalize_data = False           # If the data should be normalized before splitting into training and testing
+normalized_data_filename = "Data_Normalized.csv" # The filename to save the normalized data to
 shuffle_data = True             # If the data should be randomized before splitting into training and testing
-normalize_data = True           # If the data should be normalized before splitting into training and testing
 load_weights = False            # If the weights should be loaded from initial_weights_filename
 initial_weights_filename = "weights_initial.csv" # Filename (including path) for the weights to be saved to or read from
 should_train    = True          # If the neural network should train or just test
@@ -59,8 +60,18 @@ print(Fore.GREEN + "Complete" + Style.RESET_ALL)
 data, targets = tools.read_in_file(data_file_name, save_mapping=save_mapping, mapping_folder_name=mapping_folder)
 N, D = data.size()  # Get number of data points, N, and number of data dimensions, D.
 
+# Normalize data
+# If normalize is True, normalize data
+if normalize_data:
+    data, means, stds = tools.normalize_tensor(torch.cat((data,targets.unsqueeze(1)),1))
+    tools.tensor_to_csv(data, normalized_data_filename, fieldnames=['id','brand','model','model_year','milage','fuel_type','engine','transmission','ext_col','int_col','accident','clean_title', 'price'])
+    targets = data[:][11]
+    data = data[:][0:11]
+    
+    # targets, target_mean, target_std = tools.normalize_tensor(targets)
+
 # Split data into training_data, training_targets, test_data & test_targets
-(train_data, train_targets), (test_data, test_targets), (data, target_normalizer) = tools.split_data(data,targets, train_ratio=train_ratio, shuffle=shuffle_data, normalize=normalize_data)
+(train_data, train_targets), (test_data, test_targets) = tools.split_data(data,targets, train_ratio=train_ratio, shuffle=shuffle_data)
 
 for i in range(len(learning_rate)):
     # Initialize neural network with random weights or saved ones
@@ -82,8 +93,8 @@ for i in range(len(learning_rate)):
         print("Unclean points: " + str(n_unclean_points))
         
     # Save errors as CSV file
-    tools.error_to_csv(training_MSE_loss, ("MSE_loss_LR" + str(learning_rate[i])))
-    tools.error_to_csv(training_percent_loss, ("percent_loss_LR" + str(learning_rate[i])))
+    tools.tensor_to_csv(training_MSE_loss, ("MSE_loss_LR" + str(learning_rate[i])),fieldnames=['Data point', 'Error'])
+    tools.tensor_to_csv(training_percent_loss, ("percent_loss_LR" + str(learning_rate[i])),fieldnames=['Data point', 'Error'])
 
     # Save final weights if save weights
     if save_final_weights:

tools.py
@@ -165,9 +165,11 @@ def normalize_tensor(abnormal_tensor: torch.Tensor) -> Union [torch.Tensor, torc
     
     output:
     normal_tensor   : Size (N X D). The tensor after normalization by column
-    mean            : Size (D X 1). The means for each column of the abnormal_tensor
-    std             : Size (D X 1). The standard deviations for each column of the abnormal_tensor
+    mean            : Size (D). The means for each column of the abnormal_tensor
+    std             : Size (D). The standard deviations for each column of the abnormal_tensor
     '''
+    print("Normalizing tensor............",end="")
+
     # Get dimensions
     N, D = abnormal_tensor.shape
     
@@ -177,12 +179,26 @@ def normalize_tensor(abnormal_tensor: torch.Tensor) -> Union [torch.Tensor, torc
     # Get mean and standard deviation
     mean = torch.mean(abnormal_tensor, 0)
     std = torch.std(abnormal_tensor, 0)
+    
+    # Percent status update messaging info
+    n_loops = D*N
+    update_index = int(n_loops/100)
 
     # For each column and each row, normalize the value
-    for col in range(D):        
+    for col in range(D):
+        n_loops_done = col*N
         for row in range(N):
             normal_tensor[row, col] = (abnormal_tensor[row, col].item()-mean[col])/std[col]
-        
+
+            # Print status for every whole percent
+            loop = n_loops_done + row
+            if  loop % update_index == 0:
+                print("{:.0f} %\r".format(100*loop/n_loops),end="Normalizing tensor............")
+        # End for row
+    # End for col
+
+    print(Fore.GREEN + "Complete" + Style.RESET_ALL)
+
     # Return normal_tensor, mean and std
     return normal_tensor, mean, std
 
@@ -191,8 +207,7 @@ def split_data(
     data: torch.Tensor,
     targets: torch.Tensor,
     train_ratio: float = 0.8,
-    shuffle: bool = False,
-    normalize: bool = True) -> Union[tuple, tuple, tuple, tuple]:
+    shuffle: bool = False,) -> Union[tuple, tuple]:
     '''
     Splits the data and targets into a train and test set with the train_ratio.
     
@@ -210,10 +225,6 @@ def split_data(
     train_targets   : Size (split_index x 1). The training targets.
     test_data       : Size ((N-split_index) x D). The testing data
     test_targets    : Size ((N-split_index) x 1). The testing targets
-    data_mean       : Size (D X 1). The mean of each column of the data before normalizing
-    data_std        : Size (D X 1). The standard deviation of each column of the data before normalizing
-    target_mean     : Size (1 X 1). The mean of the target before normalizing.
-    target_std      : Size (1 X 1). The standard deviation of the target before normalizing.
     '''
     # Try except in case something goes wrong
     try:
@@ -235,14 +246,6 @@ def split_data(
             data = data[indices_new]
             targets = targets[indices_new]
 
-        # Init normalizers
-        data_normalizer = None
-        target_normalizer = None
-        # If normalize is True, normalize data
-        if normalize:
-            data, data_mean, data_std = normalize_tensor(data)
-            targets, target_mean, target_std = normalize_tensor(targets)
-
         # Find split_index between training data and test data.
         split_index = int(N * train_ratio)
 
@@ -261,7 +264,7 @@ def split_data(
         print(Fore.GREEN + "Complete" + Style.RESET_ALL)
 
         # Returned split training and testing sets
-        return (train_data, train_targets), (test_data, test_targets), (data_mean, data_std), (target_mean, target_std)
+        return (train_data, train_targets), (test_data, test_targets)
 
     # Except statement
     except Exception as e:
@@ -331,29 +334,52 @@ def tensor_to_weights(t: torch.Tensor) -> torch.nn.parameter.Parameter:
     '''
     return torch.nn.parameter.Parameter(t,requires_grad=True)
 
-def error_to_csv(loss_matrix: torch.Tensor, filename: str):
+def tensor_to_csv(tensor: torch.Tensor, filename: str, fieldnames: list=['ID', 'Value']):
     '''
-    Saves the loss matrix as a csv file
+    Saves a tensor as a csv file.
+    The first column in the csv file is the row number, ID, of the tensor and the rest of the
+    columns are the values in each column of the input tensor
     
     inputs:
-    loss_matrix : The loss matrix from NeuralNetwork.train_on_data
-    filename    : The name of the file (including path)
+    tensor      : Size (N X D), the data to be saved to a csv file
+    filename    : The name of the file (including path and file extension '.csv')
+    fieldnames  : Size (D). List of fieldnames, the header of the csv file.
     '''
-    train_cycles = loss_matrix.shape[0]
+    print("Saving to csv..................",end="")
+
+    # Get dimensions
+    N = tensor.shape[0]
+    cols = len(fieldnames)
     
+    # Percent status update messaging info
+    n_loops = cols*N
+    update_index = int(n_loops/100)
+
     # Init csv file with writing permissions and no newline at end of row for condensed data
     with open(filename, 'w', newline='') as csvfile:
-        # Header
-        fieldnames = ['Training_cycle', 'Mean_error']
         # Init writer
         writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=fieldnames)
-        # Write header with fieldnames
+        # Write header. This uses the fieldnames input
         writer.writeheader()
         # For each training cycle, write mean error
-        for cycle in range(train_cycles):
-            # Write errors to csv file
-            writer.writerow({'Training_cycle'   : cycle,
-                            'Mean_error'   : loss_matrix[cycle]})
+        for n in range(N):
+            # Init row dictionary
+            row = dict()
+            row.update({fieldnames[0]: n})
+            # Count loops
+            n_loops_done = n*cols
+            # For each field, generate row
+            for col in range(1, cols):
+                row.update({fieldnames[col]: tensor[n][col-1].item()})
+
+                # Print status for every whole percent
+                loop = n_loops_done + col
+                if  loop % update_index == 0:
+                    print("{:.0f} %\r".format(100*loop/n_loops),end="Saving to csv..................")
+
+            # Write each row to the csv file
+            writer.writerow(row)
+    print(Fore.GREEN + "Complete" + Style.RESET_ALL)
 
 # Get device for torch
 # Based on https://pytorch.org/tutorials/beginner/basics/buildmodel_tutorial.html