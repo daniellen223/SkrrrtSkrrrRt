8a8a362fc7adc82f994a47f2830edcacba053a41
G0rocks <huldarh@gmail.com>
2024-11-06 15:14:03 +0000

    Finish make_nodes.py

--- make_nodes.py
+++ make_nodes.py
@@ -1,29 +1,23 @@
 # Generate the structure for each layer with rows starting in descending order
-# Set max number of rows
-n_layers = 11
+# Set max number of layer_stacks
+n_layers = 12
 
-# For each number of hidden layers. From 1 layer to 11 layers in hidden layers
-'''
-for n_layers in range(n_rows):
-    # Init empty current_layer
-    layer_stack = []
-'''
+print("Starting")
 
 # For each version of the layer stack update it and print
-for layers_in_stack in range(2):
-    # Init empty layer_stack
-    layer_stack = []
-    # Make each node number for each layer
-    for layer_num in range(1, n_layers+1):
-        layer = []
-        for each_layer_in_stack in range(layers_in_stack):
-            # Find how many nodes should be in this layer
-            n_nodes = 2**(layer_num)
-            layer.append(n_nodes)  # Append rows as they are
-        layer_stack.append(layer)
-
+for n_layers_in_stack in range(1, n_layers):
     # Print results
-    print("Layers in stack: " + str(layers_in_stack))
-    print(layer_stack)
+    print("Layers in stack: " + str(n_layers_in_stack))
+    # Make each node number for each layer
+    for layer_num in range(1, n_layers - n_layers_in_stack + 1):
+        # Init empty layer_stack
+        layer_stack = []
+        for each_layer_in_stack in range(n_layers_in_stack):
+            # Find how many nodes should be in this layer
+            n_nodes = 2**(layer_num + n_layers_in_stack - each_layer_in_stack - 1)
+            # Append layers to layer_stack
+            layer_stack.append(n_nodes)
 
+        print(layer_stack)
 
+print("Done")
